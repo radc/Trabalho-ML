309586921a345d68f65de78a7e2992262c1d0728
Ruhan@ <ruhanconceicao@gmail.com>
2023-09-25 14:46:48 -0300

    conversor para yuv

---
+++ Scripts/y4m2yuv.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char *argv[])
+{
+    int h = 720;
+    int w = 1280;
+    char aux[25];
+    int frameCount = 0;
+    int numFrames = 600;
+
+    if (argc != 2)
+    {
+        printf("Usage: ./converter <number>\n");
+        return 1;
+    }
+
+    FILE *fptr, *dest;
+    fptr = fopen("../vidyo4_720p_60fps.y4m", "r");
+    dest = fopen("../vidyo4_720p_60fps.yuv", "w");
+
+    char c = '\0';
+
+    while(c != '\n')
+    {
+        fscanf(fptr, "%c", &c);
+    }   
+    
+    while (frameCount < numFrames){
+        fgets(aux,25,fptr); //Capture FRAME String 
+        printf("%s",aux); //Print FRAME String   
+        long counter = 0;
+        while (counter < w*h + (w*h)/2)
+        {
+            fscanf(fptr, "%c", &c);
+            fprintf(dest, "%c", c);
+            counter++;
+        }
+        frameCount++;
+    }
+    
+    fclose(fptr);
+    fclose(dest);
+
+}
