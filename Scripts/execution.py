73bdd07d6717cccf5e8c9a4bd150aa1456479e4a
Ruhan@ <ruhanconceicao@gmail.com>
2023-10-24 18:23:13 -0300

    Notebook de treino

--- Scripts/execution.py
+++ Scripts/execution.py
@@ -3,19 +3,35 @@ import os
 
 ENCODER_PATH = "./../VTM_DataExtraction/bin/EncoderAppStatic"
 AI_CFG_PATH = "../VTM_DataExtraction/cfg/encoder_intra_vtm.cfg"
-SQ_CFG_PATH = "../VTM_DataExtraction/cfg/per-sequence/Vidyo4.cfg"
-SQ_PATH = "../Sequences/vidyo4_720p_60.yuv"
 
-EXEC_STR = [ENCODER_PATH]
-EXEC_STR.append("-c")
-EXEC_STR.append(AI_CFG_PATH)
-EXEC_STR.append("-c")
-EXEC_STR.append(SQ_CFG_PATH)
-EXEC_STR.append("--InputFile="+SQ_PATH)
-EXEC_STR.append("--FramesToBeEncoded=20")
+QP=["22","27","32","37"]
+VIDEOS = ["vidyo4"]
 
-EXEC = " ".join(EXEC_STR)
-os.system(EXEC)
+SQ_CFG_DICT = {
+    "vidyo4" : "../VTM_DataExtraction/cfg/per-sequence/Vidyo4.cfg"
+    }
 
+SQ_PATH_DICT = {
+    "vidyo4" : "../Sequences/vidyo4_720p_60.yuv"
+    }
 
-# output = subprocess.call(EXEC_STR)
+# SQ_CFG_PATH = "../VTM_DataExtraction/cfg/per-sequence/Vidyo4.cfg"
+# SQ_PATH = "../Sequences/vidyo4_720p_60.yuv"
+
+os.system("rm *.csv")
+
+for video in VIDEOS :
+    for qp in QP :
+        EXEC_STR = [ENCODER_PATH]
+        EXEC_STR.append("-c")
+        EXEC_STR.append(AI_CFG_PATH)
+        EXEC_STR.append("-c")
+        EXEC_STR.append(SQ_CFG_DICT[video])
+        EXEC_STR.append("--InputFile="+SQ_PATH_DICT[video])
+        EXEC_STR.append("--FramesToBeEncoded=5")
+        EXEC_STR.append("--TemporalSubsampleRatio=1")
+        EXEC_STR.append(f"--QP={qp}")
+        EXEC_STR.append(f"> {video}_{qp}.txt")
+        EXEC = " ".join(EXEC_STR)
+        print(EXEC)
+        os.system(EXEC)
